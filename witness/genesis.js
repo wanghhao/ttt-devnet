dd8d10f882ec4831aebd6a183d9d94c7ea92517a
thing-unbutu <1558605269@qq.com>
2018-04-28 10:16:57 +0800

    修改创世区块的hash

--- witness/genesis.js
+++ witness/genesis.js
@@ -1,9 +1,10 @@
-"use strict";
+// "use strict";
 const db = require('trustnote-common/db.js');
 const headlessWallet = require('trustnote-headless');
 const eventBus = require('trustnote-common/event_bus.js');
 const constants = require('trustnote-common/constants.js');
-
+const fs = require('fs');
+const readline = require('readline');
 function onError(err) {
     throw Error(err);
 }
@@ -50,13 +51,87 @@ function createGenesisUnit(address, onDone) {
 //     db.query("INSERT INTO my_witnesses (address) VALUES (?)", [witness], onDone);    
 // }
 
+
+//yangk 20180427
+function changeGenesisHash (genesisHash){
+
+    if(typeof genesisHash == "undefined" || genesisHash == null || genesisHash == "" ){
+        console.error('genesisHash  ERROR');
+        return false ;
+    }
+    var fReadName ='../node_modules/trustnote-common/constants.js';
+    var fWriteName ='../node_modules/trustnote-common/temp_readline.js';
+    var fRead = fs.createReadStream(fReadName);
+    var fWrite = fs.createWriteStream(fWriteName);
+    var enableWriteIndex = true;
+    var index = 1;
+
+      fRead.on('end', function(){
+          enableWriteIndex = false;
+      });
+
+      var objReadline = readline.createInterface({
+          input: fRead,
+          output: fWrite
+      });
+
+      objReadline.on('line', function(line){
+          if (enableWriteIndex) {
+              if(line.indexOf('exports.GENESIS_UNIT')>=0){
+                  line = "exports.GENESIS_UNIT = '"+genesisHash+"';";
+              }
+              index ++;
+              fWrite.write(line+"\r\n");
+          }
+      });
+
+      objReadline.on('close', function(){
+          fs.unlinkSync(fReadName);
+
+          copyFile(function () {
+              fs.renameSync(fWriteName, fReadName);
+              process.exit(0);
+          });
+      });
+}
+
+function copyFile(cb) {
+
+    var fReadFile ='../node_modules/trustnote-common/temp_readline.js';
+    var fWriteTrustnoteCommon = '../../trustnote-common/constants.js';
+    var fRead = fs.createReadStream(fReadFile);
+    var fWrite = fs.createWriteStream(fWriteTrustnoteCommon);
+    var enableWriteIndex = true;
+    var index = 1;
+
+    fRead.on('end', function(){
+        enableWriteIndex = false;
+    });
+
+    var objReadline = readline.createInterface({
+        input: fRead,
+        output: fWrite
+    });
+
+    objReadline.on('line', function(line){
+        if (enableWriteIndex) {
+            index ++;
+            fWrite.write(line+"\r\n");
+        }
+    });
+    objReadline.on('close', function(){
+        cb();
+    });
+}
+
 eventBus.once('headless_wallet_ready', function() {
     headlessWallet.readSingleAddress(function(address) {
         createGenesisUnit(address, function(genesisHash) {
             console.log("Genesis created, hash=" + genesisHash);
-            // addMyWitness(address, function() {
-                process.exit(0);
-            // });
+            changeGenesisHash(genesisHash);
         });
     });
 });
+
+
+
